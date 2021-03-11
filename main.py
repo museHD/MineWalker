c6920ccf7d442d39b51531b577feaf7fdd8983b9
MuseHD <57558058+museHD@users.noreply.github.com>
2021-03-12 00:22:46 +1100

    added comments and added boxes to 2d array

--- main.py
+++ main.py
@@ -2,23 +2,40 @@ import itertools
 import pandas
 import numpy as np
 from random import randint
+
 class Game(object):
 
 	def __init__(self):
 		self.length = 10
 
+	# Takes a 2D array, converts it to a pandas df and prints it to display
+	# TODO: Find out if the entire grid can be reprinted in console
+	def print_grid(self, grid):
+		df = pandas.DataFrame(grid)
+		print(df.to_string(index=False, header=False))
+
+	# Generates a simple 2d array
 	def gen_grid(self):
+
 		grid = []
 		thisRow = []
+		# Box unicode 
+		boxChar = "\u2610"
+
+		# Populates a 2D array with box characters
 		for yCols in range(self.length):
 			for xRows in range(self.length):
-				thisRow.append(0)
+				thisRow.append(boxChar)
 			grid.append(thisRow)
 			thisRow = []
-		print(grid)
+		return grid
+
+
 myGame = Game()
 myGame.gen_grid()
 
+
+""" SNIPPET using numpy """
 # def print_array(array):
 # 	for y in range(len(array)):
 # 		for x in range(len(array)):
